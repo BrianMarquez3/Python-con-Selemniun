ed8dbc0842b998f4f8991ea029bfb5eaa2317fb6
BrianMarquez3 <brian3marquez@gmail.com>
2020-05-18 22:46:32 -0500

    FLGKDFG

--- 1. Ingreso de datos.py
+++ 1. Ingreso de datos.py
@@ -5,14 +5,17 @@ from selenium.webdriver.common.keys import Keys
 import time 
 
 driver = webdriver.Chrome(executable_path=r"D:\driverchrome\chromedriver.exe")#Ubicacion de Driver de Chrome
+#driver = webdriver.Firefox(executable_path=r"D:\drivermozila\geckodriver.exe")#Ubicacion de Driver de Chrome
+
 driver.get("https://www.gmail.com")
 
 usuario = driver.find_element_by_id("identifierId")# Se econtro el componente
-usuario.send_keys("registropsicoune@gmail.com")
+usuario.send_keys("tucorreo@gmail.com")
 usuario.send_keys(Keys.ENTER)
 time.sleep(3)
 
 clave = driver.find_element_by_name("password")
-clave.send_keys("23mner56")
+clave.send_keys("tucontraseña")
 clave.send_Keys(Keys.ENTER)
-#iNGRESAR A Gamil Automatiocamente
+#Ingresar A Gmail Automatiocamente
+#
