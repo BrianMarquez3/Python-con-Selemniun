1513ca356b1ecef4bdddbdff9e5a93b30a13b755
Brian Enrique Marquez Inca Roca <brian3marquez@outlook.com>
2020-06-15 16:21:00 -0500

    test

--- Toggle o Switch/Toggle_switch.py
+++ Toggle o Switch/Toggle_switch.py
@@ -1,17 +1,15 @@
 # Boton On/Off
 
+
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
 class usando_unittest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path=r"D:\Automatizacion\driverchrome\chromedriver.exe")
 
-    def test_usando_opencv(self):
-        driver = self.driver
+    def test_usando_toggle(self):
+        driver = self.driver #llamda al driver
         driver.get("https://www.google.com")
-        driver.save_screenshot('img2.png') #hacer recorte
-        time.sleep(3)
-
+        
