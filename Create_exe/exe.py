62d9c07fd32c0ce03b911c17d1801b607286b705
BrianMarquez3 <brian3marquez@gmail.com>
2020-06-22 21:02:54 -0500

    Pyinstaller

---
+++ Create_exe/exe.py
@@ -0,0 +1,11 @@
+#Crear un Exe en python
+#Automaticacion con IE
+#Creacion de un .exe
+
+from selenium import webdriver # se Improta libreria
+import time
+
+driver = webdriver.Ie(executable_path=r"D:\Automatizacion\IEDriverServer.exe")
+driver.get("https://www.google.com")
+time.sleep(3)
+driver.close()
